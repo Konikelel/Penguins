9ab3afc0bf0e874bf7344fd636127c0b84e0dad9
Konikelel <wiktoralbinowski@gmail.com>
2023-11-29 22:35:49 +0100

    optimize spawnPenguins, movePenguins

--- Game/PlayerAction.c
+++ Game/PlayerAction.c
@@ -15,21 +15,21 @@ void spawnPenguin(struct Player *pPlayer, struct Board *pBoard) {
 
     while (1) {
         printf("\nP%d Choose penguin spawn:\n", pPlayer->id);
-
+        // Get coordinates of penguin
         tileChoosen = askForCoordinates(pBoard);
-
+        // Check if tile has one fish, isn't blocked and isn't removed
         if (isSpawnValid(tileChoosen))
             break;
-
+        // Show board
         pBoard->show(pBoard);
         printError("\nInvalid coordinates!");
     }
-
+    // Set isOccupied of chosen tile with playerID
     tileChoosen->isOccupied = pPlayer->id;
-
+    // Set player penguin pointer to the chosen tile and add fishes that were collected by player
     pPlayer->pPenguins[*nrPlayerPenguins] = tileChoosen;
     pPlayer->collectedFish += tileChoosen->nrFish;
-
+    // Increment the number of penguins that player placed
     (*nrPlayerPenguins)++;
 }
 
@@ -39,18 +39,17 @@ void movePenguin(struct Player *pPlayer, struct Board *pBoard) {
 
     while (1) {
         printf("\nP%d Choose penguin:\n", pPlayer->id);
-
+        // Get penguins coordinates
         pTileActive = askForCoordinates(pBoard);
 
-        if (!isPlayerPenguin(pPlayer, pTileActive)) {
-            pBoard->show(pBoard);
+        pBoard->show(pBoard);
+        // Check if penguin belongs to player
+        if (!isPlayerPenguin(pPlayer, pTileActive))
             printError("\nInvalid coordinates!");
-        }
 
-        else if (isSetTileBlocked(pBoard, pTileActive)) {
-            pBoard->show(pBoard);
+        // Check if tile is blocked or removed
+        else if (isSetTileBlocked(pBoard, pTileActive))
             printError("\nPenguin is blocked!");
-        }
 
         else
             break;
@@ -61,36 +60,36 @@ void movePenguin(struct Player *pPlayer, struct Board *pBoard) {
 
     while (1) {
         printf("\nP%d Choose where to move penguin:\n", pPlayer->id);
-
+        // Get coordinates of move
         pTileSet = askForCoordinates(pBoard);
 
-        if (!isMoveInOneDimension(pBoard, pTileActive, pTileSet)) {
-            pBoard->show(pBoard);
+        pBoard->show(pBoard);
+        // Check if move is in one dimension
+        if (!isMoveInOneDimension(pBoard, pTileActive, pTileSet))
             printError("\nMove is not in one dimension!");
-        }
-
-        else if (!isRoadClear(pBoard, pTileActive, pTileSet)) {
-            pBoard->show(pBoard);
+        // Check if path from penguin to the tile doesn't have any tiles removed or with penguin
+        else if (!isRoadClear(pBoard, pTileActive, pTileSet))
             printError("\nRoad is not clear!");
-        }
 
         else
             break;
     }
-
+    // Add fishes that player collected
     pPlayer->collectedFish += pTileSet->nrFish;
-
+    // Loop over player's penguins
     for (int nr = 0; nr < 4; nr++) {
+        // Check if penguin and tile pointers values are the same
         if (pPlayer->pPenguins[nr] == pTileActive) {
+            // Set penguin pointer to tile where penguin moved
             pPlayer->pPenguins[nr] = pTileSet;
             break;
         }
     }
-
+    // Set tile were was penguin to remove
     pTileActive->isOccupied = 0;
     pTileActive->isActive = 0;
     pTileActive->isRemoved = 1;
-
+    // Set tile where penguin moved to occupied by player and add collect fishes
     pTileSet->isOccupied = pPlayer->id;
     pPlayer->collectedFish += pTileSet->nrFish;
 }
@@ -98,11 +97,14 @@ void movePenguin(struct Player *pPlayer, struct Board *pBoard) {
 struct Tile *askForCoordinates(struct Board *pBoard) {
     int x, y;
 
+    // Input X coordinate, check if x is within the range
     do {
         printf("- Enter X coordinate: ");
         scanf("%d", &x);
+
     } while (x < 0 || x > pBoard->nrColumns);
 
+    // Input X coordinate, check if y is within the range
     do {
         printf("- Enter Y coordinate: ");
         scanf("%d", &y);
@@ -112,7 +114,9 @@ struct Tile *askForCoordinates(struct Board *pBoard) {
 }
 
 bool isPlayerPenguin(struct Player *pPlayer, struct Tile *pTile) {
+    // Go over each of player's penguins
     for (int nr = 0; nr < pPlayer->usedPenguins; nr++)
+        // Check if penguin and tile are pointing to the same value
         if (pPlayer->pPenguins[nr] == pTile)
             return true;
     return false;
@@ -122,10 +126,13 @@ bool canPlayerMoveAnyPenguin(struct GameData *pGameData, int playerId) {
     struct Tile **pTiles = pGameData->pPlayers[playerId].pPenguins;
     int nrStuckPenguins = 0;
 
+    // Go over each of player's penguins
     for (int nr = 0; nr < pGameData->nrPenguinsPerPlayer; nr++) {
+        // Check if tile is blocked and increment when it is
         if (isSetTileBlocked(pGameData->pBoard, pTiles[nr]))
             nrStuckPenguins++;
     }
 
+    // Return if number of stuck penguins is not equal to number of player's penguin
     return !(nrStuckPenguins == pGameData->nrPenguinsPerPlayer);
 }
